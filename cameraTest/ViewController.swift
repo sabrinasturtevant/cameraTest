48dff286cd0ed53e49ce97f6f22dfd25f963e1f8
Sabrina Sturtevant <sabrina.sturteva@smail.astate.edu>
2019-10-01 17:38:34 -0500

    Initial Commit

---
+++ cameraTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cameraTest
+//
+//  Created by Sabrina Sturtevant on 10/1/19.
+//  Copyright © 2019 Sabrina Sturtevant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
